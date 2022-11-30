c2aba3d2daf3e56ac174ff36fbd7db3d502b12bc
mikhael <mikhaelsiman2003@gmail.com>
2022-11-30 14:57:01 +0700

    add some file quotesmodel and retroserver

---
+++ app/src/main/java/com/mikhael/if3b/quotes/API/RetroServer.java
@@ -0,0 +1,13 @@
+package com.mikhael.if3b.quotes.API;
+
+import retrofit2.Retrofit;
+
+public class RetroServer {
+    private static final String baseURL = "https://type.fit/api/";
+    private  static Retrofit retro;
+
+    public static Retrofit connectRetrofit() {
+
+    }
+
+}
